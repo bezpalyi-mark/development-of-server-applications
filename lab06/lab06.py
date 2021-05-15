d1f92b23ea5a0719f07462a196dcb415396bbc46
Mark Bezpalyi <mark.bezpalyi@nixsolutions.com>
2021-05-15 23:13:30 +0300

    Lab 6 end

--- lab06/lab06.py
+++ lab06/lab06.py
@@ -1,8 +1,20 @@
+import uuid
+
+
+def century_from_year(year):
+    return year // 100 + 1
+
+
 class SimpleElement:
     value = 0
+    id = 0
 
     def __init__(self, value):
         self.value = value
+        self.id = uuid.uuid4()
+
+    def __repr__(self):
+        return f"Value:{self.value}, ID:{self.id}"
 
     def __eq__(self, other):
         if isinstance(other, SimpleElement):
@@ -10,6 +22,9 @@ class SimpleElement:
                 return True
             else:
                 return False
+
+    def __hash__(self):
+        return hash(self.value)
 
     def bigger_than(self, other):
         if isinstance(other, SimpleElement):
@@ -33,7 +48,7 @@ class Utils:
     # to left of pivot and all greater elements to right
     # of pivot
 
-    def partition(self, arr, low, high):
+    def __partition(self, arr, low, high):
         i = (low - 1)  # index of smaller element
         pivot = arr[high]  # pivot
 
@@ -41,7 +56,7 @@ class Utils:
 
             # If current element is smaller than or
             # equal to pivot
-            if arr[j] <= pivot:
+            if arr[j].smaller_then(pivot) or arr[j].__eq__(pivot):
                 # increment index of smaller element
                 i = i + 1
                 arr[i], arr[j] = arr[j], arr[i]
@@ -62,23 +77,65 @@ class Utils:
         if low < high:
             # pi is partitioning index, arr[p] is now
             # at right place
-            pi = self.partition(arr, low, high)
+            pi = self.__partition(arr, low, high)
 
             # Separately sort elements before
             # partition and after partition
             self.quick_sort(arr, low, pi - 1)
             self.quick_sort(arr, pi + 1, high)
 
-    def index_of(self, arr, element):
-        if isinstance(arr, list):
-            arr.index(element)
+    def find_first_by_value(self, arr, value):
+        for el in arr:
+            if el.value == value:
+                return el
+
+    def __find_min(self, arr):
+        return min(list(map(lambda el: el.value, arr)))
+
+    def __find_max(self, arr):
+        return max(list(map(lambda el: el.value, arr)))
+
+    def __find(self, finder, arr, size):
+        search_value = finder(arr)
+        entries = []
+        if size > len(arr) - 1:
+            size = len(arr) - 1
+        for el in arr:
+            if len(entries) < size:
+                if el.value == search_value:
+                    entries.append(el)
+            else:
+                return entries
+        return entries
+
+    def find_with_min_value(self, arr, size):
+        return self.__find(self.__find_min, arr, size)
+
+    def find_with_max_value(self, arr, size):
+        return self.__find(self.__find_max, arr, size)
+
+    def get_average_value(self, arr):
+        return sum(map(lambda el: el.value, arr)) / len(arr)
+
+    def find_all_distinct(self, arr):
+        return list(set(arr))
 
 
-# Driver code to test above
 utils = Utils()
-arr_test = [SimpleElement(10), SimpleElement(2), SimpleElement(-5), SimpleElement(13), SimpleElement(0)]
+arr_test = [SimpleElement(10), SimpleElement(2), SimpleElement(2), SimpleElement(-5), SimpleElement(13),
+            SimpleElement(0), SimpleElement(-5), SimpleElement(13), SimpleElement(13)]
 n = len(arr_test)
 utils.quick_sort(arr_test, 0, n - 1)
 print("Sorted array is:")
 for element in range(n):
-    print("%d" % arr_test[element])
+    print("%d" % arr_test[element].value)
+print(f"Search element with value 10: {utils.find_first_by_value(arr_test, 10)}")
+print(f"Search 1 min element: {utils.find_with_min_value(arr_test, 1)}")
+print(f"Search 3 min element: {utils.find_with_min_value(arr_test, 3)}")
+print(f"Search 1 max element: {utils.find_with_max_value(arr_test, 1)}")
+print(f"Search 3 max element: {utils.find_with_max_value(arr_test, 3)}")
+print(f"Average value: {utils.get_average_value(arr_test)}")
+print(f"Distinct elements: {utils.find_all_distinct(arr_test)}")
+print(f"Century of 2001: {century_from_year(2021)}\n"
+      f"Century of 1995: {century_from_year(1995)}\n"
+      f"Century of 1234: {century_from_year(1234)}")
